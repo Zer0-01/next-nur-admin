1a060dc994afa769b522fce407eca008359d615c
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-03-30 10:14:29 +0800

    first commit

---
+++ src/app/masjid/page.tsx
@@ -0,0 +1,13 @@
+import { Button, Container, Row } from "react-bootstrap";
+
+const Masjid = () => {
+    
+
+    return (
+        <Container>
+            <Button variant="primary">Add Masjid</Button>
+        </Container>
+    );
+};
+
+export default Masjid;
