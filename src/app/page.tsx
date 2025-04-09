830c4cf83c0eeb649d0647949cc4ffe0638e977b
Zer0-01 <anas.zulkifli2001@gmail.com>
2025-04-09 23:51:09 +0800

    feat: create table and add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Col, Container, Row, Table } from "react-bootstrap";
 
 export default function Page() {
     return <>
         <Container>
             <h1>Doa</h1>
+
+            <Row className="justify-content-end mb-1">
+                <Col xs="auto">
+                    <Button className="rounded-circle" variant="primary">+</Button>
+                </Col>
+
+            </Row>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan={2}>Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
         </Container>
     </>;
 }
